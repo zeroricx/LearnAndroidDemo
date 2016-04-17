7f7831ea557d2f502d4e030d373f833b93851a50
zeroricx <zeroricx@gmail.com>
2016-04-17 12:47:52 +0800

    update

--- app/src/main/kotlin/com/zeroricx/StatusBar/StatusBar.kt
+++ app/src/main/kotlin/com/zeroricx/StatusBar/StatusBar.kt
@@ -1,1 +1,51 @@
 package com.zeroricx.StatusBar
+
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.LinearLayout
+
+object StatusBar {
+    val DEFAULT_ALPHA = 112
+
+    @JvmOverloads
+    fun setColor(activity: Activity, color: Int, alpha: Int = DEFAULT_ALPHA) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            activity.window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            activity.window.statusBarColor = calculateStatusColor(color, alpha)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            activity.window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            val statusBar = creatStatusBar(activity, color, alpha)
+        }
+    }
+
+    private fun creatStatusBar(activity: Activity, color: Int, alpha: Int): View {
+        val statusBarView = View(activity)
+        val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                getStatusBarHeight(activity))
+        statusBarView.layoutParams = params
+        statusBarView.setBackgroundColor(calculateStatusColor(color, alpha))
+        return statusBarView
+    }
+
+    private fun getStatusBarHeight(context: Context): Int {
+        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+        return context.resources.getDimensionPixelSize(resourceId)
+    }
+
+    //计算颜色值
+    private fun calculateStatusColor(color: Int, alpha: Int): Int {
+        val a = 1 - alpha / 255f
+        var red = color shr 16 and 0xff
+        var green = color shr 8  and 0xff
+        var blue = color and 0xff
+        red = (red * a + 0.5).toInt()
+        green = (green * a + 0.5).toInt()
+        blue = (blue * a + 0.5).toInt()
+        return 0xff shl 24 or red shl 16 or green shl 8 or blue
+    }
+}
